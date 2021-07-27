70efdbb91ab43aa166f5d88addbdbdc916f6b275
Ralph <rshipard@gmail.com>
2021-07-27 22:32:11 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+
+app.listen(port, ()=> {
+    console.log(`*** Listening on port ${port} ***`);
+});
+
+app.get('/', (req, res)=> {
+    const testing123 = {"testing":"Mike test, 1... 2?"}
+    res.json(testing123);
+});
