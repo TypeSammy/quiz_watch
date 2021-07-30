884308cf7c9ae969bc647c25d98113acd16a51fe
Samantha <typesammy@gmail.com>
2021-07-30 14:46:23 +1000

    landing page base complete

--- client/scripts/header_nav.js
+++ client/scripts/header_nav.js
@@ -1,15 +1,15 @@
 function renderHeaderNav() {
   document.querySelector("#header-nav").innerHTML = `
     <nav>
-      <h1>Quiz Watch</h1>
+      <h3>Quiz Watch</h3>
       <ul>
         <div>
           <li class="material-icons account_circle" onClick="render('Signup')">account_circle</li>
-          <li class="nav-text" onClick="render('Signup')>Signup</li>
+          <li class="nav-text" onClick="render('Signup')">Signup</li>
         </div>
         <div>
-          <li class="material-icons login" onClick="render('Login')>login</li>
-          <li class="nav-text" onClick="render('Login')>Login</li>
+          <li class="material-icons login" onClick="render('Login')">login</li>
+          <li class="nav-text" onClick="render('Login')">Login</li>
         </div>
       </ul>
     </nav>
