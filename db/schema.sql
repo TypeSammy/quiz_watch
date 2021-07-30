ada6f55e4a66ea7e76a5091c440222368c09b633
Kevin <kevin.vu06@gmail.com>
2021-07-31 09:56:50 +1000

    db update

--- db/schema.sql
+++ db/schema.sql
@@ -14,15 +14,29 @@ question TEXT,
 hint TEXT,
 answer TEXT,
 answered_correctly INT,
-answered_incorrectly INT
+answered_incorrectly INT,
+reminder TIMESTAMP,
+difficulty INT[], 
+category TEXT
 );
 
-CREATE TABLE quiz_reminders (
-id SERIAL PRIMARY KEY,
-user_id INT,
-flashcard_id INT,
-reminder TIMESTAMP
-);
+-- DUMMY DATA to test 
+
+INSERT INTO flashcards (user_id, question, hint, answer, answered_correctly, answered_incorrectly, reminder, category)
+VALUES (1, 'In JS how do you remove whitespace', 'for example shrek 1 ', 'console.log("shrek 1".trim)', 0, 0, now(), 'Javascript' );
+
+INSERT INTO flashcards (user_id, question, hint, answer, answered_correctly, answered_incorrectly, reminder, category)
+VALUES (2, 'In JS how do you remove whitespace', 'for example shrek 1 ', 'console.log("shrek 1".trim)', 0, 0, now(), 'Javascript' );
+
+INSERT INTO flashcards (user_id, question, hint, answer, answered_correctly, answered_incorrectly, reminder, category)
+VALUES (1, 'In JS how do you remove whitespace', 'for example shrek 1 ', 'console.log("shrek 1".trim)', 0, 0, timestamp '2030-09-28 23:00', 'Javascript' );
+
+
+-- SELET THE flashcards due for all users
+SELECT * FROM flashcards where reminder < now()
+
+-- Update flashcards
+UPDATE flashcards SET reminder = '2030-09-28 22:00' WHERE id = 2
 
 -- NOTES:
 -- Remove table data without dropping the table, better than DELETE as it resets the table auto-increment value:
