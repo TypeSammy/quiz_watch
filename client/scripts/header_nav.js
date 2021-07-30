a9ec58410067fca4716d43d3986d5e101f16d6fc
Samantha <typesammy@gmail.com>
2021-07-30 18:11:52 +1000

    added signup and login form

--- client/scripts/header_nav.js
+++ client/scripts/header_nav.js
@@ -4,15 +4,23 @@ function renderHeaderNav() {
       <h3>Quiz Watch</h3>
       <ul>
         <div>
-          <li class="material-icons account_circle" onClick="render('Signup')">account_circle</li>
-          <li class="nav-text" onClick="render('Signup')">Signup</li>
+          <li class="material-icons signup" onClick="render('signup')">account_circle</li>
+          <li class="nav-text" onClick="render('signup')">Sign up</li>
         </div>
         <div>
-          <li class="material-icons login" onClick="render('Login')">login</li>
-          <li class="nav-text" onClick="render('Login')">Login</li>
+          <li class="material-icons login" onClick="render('login')">login</li>
+          <li class="nav-text" onClick="render('login')">Login</li>
         </div>
       </ul>
     </nav>
   `
 }
 renderHeaderNav()
+
+function render(click) {
+  if (click === "signup") {
+    renderSignup()
+  } else if (click === "login") {
+    renderLogin()
+  }
+}
