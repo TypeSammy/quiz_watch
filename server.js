adca39b7caa6c57f3d97f292a582fce9fd18db9d
Samantha <typesammy@gmail.com>
2021-07-30 11:49:29 +1000

    amended server file to include logger and connected it all to the client folder

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
-
 const port = 3000
 
-app.use(express.json())
+// require middlewares:
+const logger = require("./middlewares/logger.js")
 
 app.listen(port, ()=> {
-    console.log(`*** Listening on port ${port} ***`);
-});
+    console.log(`*** Listening on port ${port} ***`)
+})
+
+app.use(logger)
+app.use(express.static("client"))
+app.use(express.json())
+
 
 app.get('/', (req, res)=> {
     const testing123 = {"testing":"Mike test, 1... 2?"}
-    res.json(testing123);
-});
+    res.json(testing123)
+})
