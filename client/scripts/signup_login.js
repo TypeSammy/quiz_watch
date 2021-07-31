2f87245f90e80c3de3b835af4a7831865682c9d4
Samantha <typesammy@gmail.com>
2021-07-31 16:06:15 +1000

    refactored some render files

--- client/scripts/signup_login.js
+++ client/scripts/signup_login.js
@@ -1,57 +1,63 @@
 const login = document.querySelector("#login")
 const signup = document.querySelector("#signup")
 
-function renderSignup() {
-  const signupBox = document.querySelector("#signup")
-  signupBox.classList.remove("hide")
-  signupBox.innerHTML = `
-    <div class="signup-container">
-      <h2>Signup</h2>
-      <form id="sign-up-form" action="/" method="POST">
-        <fieldset>
-          <input type="text" name="name" placeholder="Username">
-        </fieldset>
-        <fieldset>
-          <input type="text" name="email" placeholder="Email">
-        </fieldset>
-        <fieldset>
-          <input type="password" name="password" placeholder="Password">
-        </fieldset>
-        <button>Sign up</button>
-      </form>
-      <span class="material-icons signup" onClick="render('signup')">close</span>
-    </div>
-  `
-}
-renderSignup()
-signup.classList.toggle("hide")
-
-function renderLogin() {
-  const signupBox = document.querySelector("#login")
-  signupBox.classList.remove("hide")
-  signupBox.innerHTML = `
-    <div class="login-container">
-      <h2>Login</h2>
-      <form id="login-up-form" action="/" method="POST">
-        <fieldset>
-          <input type="text" name="name" placeholder="Username">
-        </fieldset>
-        <fieldset>
-          <input type="password" name="password" placeholder="Password">
-        </fieldset>
-        <button>Login</button>
-      </form>
-      <span class="material-icons login" onClick="render('login')">close</span>
-    </div>
-  `
-}
-renderLogin()
-login.classList.toggle("hide")
-
-function render(component) {
-  if (component === "signup") {
-    signup.classList.toggle("hide")
-  } else if (component === "login") {
-    login.classList.toggle("hide")
+const LoginSignup = (() => {
+  function renderSignup() {
+    const signupBox = document.querySelector("#signup")
+    signupBox.classList.remove("hide")
+    signupBox.innerHTML = `
+      <div class="signup-container">
+        <h2>Signup</h2>
+        <form id="sign-up-form" action="/" method="POST">
+          <fieldset>
+            <input type="text" name="name" placeholder="Username">
+          </fieldset>
+          <fieldset>
+            <input type="text" name="email" placeholder="Email">
+          </fieldset>
+          <fieldset>
+            <input type="password" name="password" placeholder="Password">
+          </fieldset>
+          <button>Sign up</button>
+        </form>
+        <span class="material-icons signup" onClick="LoginSignup.hide('signup')">close</span>
+      </div>
+    `
   }
-}
+  function renderLogin() {
+    const signupBox = document.querySelector("#login")
+    signupBox.classList.remove("hide")
+    signupBox.innerHTML = `
+      <div class="login-container">
+        <h2>Login</h2>
+        <form id="login-up-form" action="/" method="POST">
+          <fieldset>
+            <input type="text" name="name" placeholder="Username">
+          </fieldset>
+          <fieldset>
+            <input type="password" name="password" placeholder="Password">
+          </fieldset>
+          <button>Login</button>
+        </form>
+        <span class="material-icons login" onClick="LoginSignup.hide('login')">close</span>
+      </div>
+    `
+  }
+  renderSignup()
+  renderLogin()
+  signup.classList.toggle("hide")
+  login.classList.toggle("hide")
+
+  function hide(component) {
+    if (component === "signup") {
+      signup.classList.toggle("hide")
+    } else if (component === "login") {
+      login.classList.toggle("hide")
+    }
+  }
+
+  return {
+    hide: hide
+  }
+
+})()
