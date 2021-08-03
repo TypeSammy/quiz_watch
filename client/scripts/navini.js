525ab140cc6ddece045c2264131988dfc98e5def
Kevin <kevin.vu06@gmail.com>
2021-08-03 19:51:17 +1000

    user function finished

--- client/scripts/navini.js
+++ client/scripts/navini.js
@@ -3,3 +3,28 @@ const state = {
   flashcardsdue: [],
   allFlashcards: []
 }
+
+function isUserLoggedin() {
+	axios.get("/api/sessions")
+		// .then(successResponse => {
+    //   console.log(successResponse) // TEST
+    //   state.logInStatus = true
+    //   Nav.header()
+    //   login.classList.toggle("hide")
+    //   console.log(state.logInStatus)
+		// })
+    .then(successResponse => {
+      axios.get('/api/quiz')
+      .then(response => {
+        state.flashcardsdue = response.data
+      })
+    })
+    .then(successResponse => {
+      axios.get('/api/quiz/all')
+      .then(response => {
+        state.allFlashcards = response.data
+      })
+    })
+
+  }
+isUserLoggedin()
