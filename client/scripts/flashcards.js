e5a000be72d737cd62dba5fb63c2ec6028ce8a29
Kevin <kevin.vu06@gmail.com>
2021-08-05 13:09:58 +1000

    reminder function working

--- client/scripts/flashcards.js
+++ client/scripts/flashcards.js
@@ -44,15 +44,15 @@ function getFlashcard(){
 
 }
 
-function reminderUpdate(difficulty, id){
-  var updatedTime = moment().format(`YYYY-MM-DD HH:mm:ss`)
+// function reminderUpdate(difficulty, id){
+//   var updatedTime = moment().format(`YYYY-MM-DD HH:mm:ss`)
 
-  if (difficulty == 'easy'){
-    var updatedTime = moment().add(7, 'd').format(`YYYY-MM-DD HH:mm:ss`)
-  } else if(difficulty == 'medium'){
-    var updatedTime = moment().add(1, 'h').format(`YYYY-MM-DD HH:mm:ss`)
-  } else if(difficulty == 'hard'){
-    var updatedTime = moment().add(1, 'm').format(`YYYY-MM-DD HH:mm:ss`)
-  } 
-  axios.patch('/api/quiz',updatedTime, id)
-}
+//   if (difficulty == 'easy'){
+//     var updatedTime = moment().add(7, 'd').format(`YYYY-MM-DD HH:mm:ss`)
+//   } else if(difficulty == 'medium'){
+//     var updatedTime = moment().add(1, 'h').format(`YYYY-MM-DD HH:mm:ss`)
+//   } else if(difficulty == 'hard'){
+//     var updatedTime = moment().add(1, 'm').format(`YYYY-MM-DD HH:mm:ss`)
+//   } 
+//   axios.patch('/api/quiz',updatedTime, id)
+// }
--- client/scripts/flashcards_test.js
+++ client/scripts/flashcards_test.js
@@ -9,14 +9,12 @@ function flashcardDOM() {
 }
 
 function grabFlashcardsDue() {
-  console.log('TEST 1')
   return state.flashcardsdue
 }
 
 let currentCardIndex = null
 
 function start() {
-  console.log('TEST 2')
   let dom = flashcardDOM()
   // let flashcardsDue = grabFlashcardsDue()
   document.querySelector(".start-btn").style.display = "none"
@@ -30,13 +28,11 @@ function start() {
 }
 
 function nextQ() {
-  console.log('TEST 3')
   let flashcardsDue = grabFlashcardsDue()
   showCard(flashcardsDue[currentCardIndex])
 }
 
 function showCard(flashcardsDue) {
-  console.log('TEST 4')
   let dom = flashcardDOM()
   dom.displayQContainer.setAttribute("data-id", flashcardsDue.id)
   dom.question.innerText = flashcardsDue.question
@@ -57,7 +53,6 @@ function showAnswer(flashcardsDue) {
 }
 
 function difficulty(lvl) {
-  console.log('TEST 5')
   let dom = flashcardDOM()
   let cardId = dom.displayQContainer.getAttribute("data-id")
   // dom object for question page
@@ -68,11 +63,39 @@ function difficulty(lvl) {
   // dom object for answer page
   dom.answer.style.display = "none"
   dom.reminderBtnContainer.style.display = "none"
+// functions to add minute or days
+function addDays(date, days) {
+  date.setDate(date.getDate() + days)
+  return date;
+}
+function addMinutes(date, minutes) {
+  date.setTime(date.getTime() + minutes*60000)
+  return date;
+}
+// 
+  var someDate = new Date();
+  if (lvl == 'easy'){ addDays(someDate, 7) } 
+    else if( lvl == 'medium') { addDays(someDate, 1) }
+    else if (lvl == 'hard') { addMinutes(someDate, 1) } 
 
-  // changes the index to cycle through array
-  reminderUpdate(lvl, cardId)
+  var dd = someDate.getDate();
+  var mm = someDate.getMonth() + 1;
+  var y = someDate.getFullYear();
+  var hour = someDate.getHours();
+  var minutes = someDate.getMinutes();
+  var seconds = someDate.getSeconds();
+
+  var reminder = y + '/'+ mm + '/'+ dd + " " + hour + ":" + minutes + ":" + seconds;
+
+  axios.patch('/api/quiz',{timestamp: reminder, id: cardId} )
+   // changes the index to cycle through array
   currentCardIndex+=1
   nextQ()
+}
+
+function createNewFlashcard(question, hint, answer){
+  // need to grab the data from the user submitted form
+  axios.post('/api/quiz',{question: question, hint: hint, answer: answer} )
 }
 
 const fakeQuestions = [
