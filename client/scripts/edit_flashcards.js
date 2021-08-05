19ce09c94c0231fb6786efe3566787d0f5a16706
Kevin <kevin.vu06@gmail.com>
2021-08-06 08:07:18 +1000

    edit DOM completed - toggle not done

--- client/scripts/edit_flashcards.js
+++ client/scripts/edit_flashcards.js
@@ -34,7 +34,7 @@ const editFlashcard = document.querySelector("#flashcard")
 
 // })()
 
-function renderAllFlashcards() {
+/*function renderAllFlashcards() {
   document.querySelector("#all-flashcards").innerHTML = `
     <section class="all-questions-display-container">
       <div class="question-container">
@@ -80,7 +80,8 @@ function renderAllFlashcards() {
       </div>
     </section>
 `
-}
+}*/
+
 
 function renderFlashcard() {
   document.querySelector("#flashcard").innerHTML = `
@@ -97,6 +98,58 @@ function renderFlashcard() {
         <fieldset>
           <label for="question">Answer:</label><br>
           <textarea name="answer" cols="20" rows="5">Answer information display here</textarea>
+        </fieldset>
+        <button>Save</button>
+        <button class="delete">Delete</button>
+        </form>
+    </div>
+  `
+}
+
+
+
+function renderAllFlashcards() {
+  document.querySelector("#all-flashcards").innerHTML = `
+    <section class="all-questions-display-container">
+      ${displayAllFlashcards()}
+    </section>
+
+`
+}
+
+
+
+function displayAllFlashcards() {
+  return state.allFlashcards.map(flashcard => `
+    <section class="question-container" data-id=${flashcard.id}>
+        <div class="question-preview">${flashcard.question}</div>
+        <div class="edit-container">
+          <span class="material-icons" onClick="Toggle.editFlashcard(); Toggle.editAllFlashcards()">edit</span><div class="edit" onClick="Toggle.editFlashcard(); Toggle.editAllFlashcards();mapFlashcard(event)">Edit</div>
+        </div>
+      </section>
+    `).join('')
+}
+
+
+function mapFlashcard(event) {
+  const editFlashcardId = event.target.closest('.question-container').dataset.id
+  var converted = Object.assign({}, ...state.allFlashcards.map(object => ({[object.id]: object})))
+  var flashcardToBeEdited = converted[editFlashcardId]
+
+  document.querySelector("#flashcard").innerHTML = `
+    <div class="edit-flashcard-container">
+      <form id="edit-flashcard-form" action="/" method="POST">
+        <fieldset>
+          <label for="question">Question:</label><br>
+          <textarea name="question" cols="20" rows="5">${flashcardToBeEdited.question}</textarea>
+        </fieldset>
+        <fieldset>
+          <label for="question">Hint:</label><br>
+          <textarea name="hint" cols="20" rows="5">${flashcardToBeEdited.hint}</textarea>
+        </fieldset>
+        <fieldset>
+          <label for="question">Answer:</label><br>
+          <textarea name="answer" cols="20" rows="5">${flashcardToBeEdited.answer}</textarea>
         </fieldset>
         <button>Save</button>
         <button class="delete">Delete</button>
